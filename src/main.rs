241205fbc9c865ca1c1e4338c244612e1c835210
Bart Massey <bart.massey@gmail.com>
2023-08-15 14:25:40 -0700

    started spi

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,21 @@ use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
 
 use cortex_m_rt::entry;
-use microbit::board::Board;
+use microbit::{
+    board::Board,
+    hal::{spi::Spi, spim},
+    pac::spim0,
+};
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
-    let _board = Board::take().unwrap();
-    let mut counter = 0u64;
-    loop {
-        rprintln!("{}", counter);
-        counter += 1;
-    }
+    let board = Board::take().unwrap();
+    let spi_mode = spim::Mode {
+        polarity: spim::Polarity::IdleLow,
+        phase: spim::Phase::CaptureOnFirstTransition,
+    };
+    // XXX Can probably be much faster.
+    let spi_clock_rate = spim0::FREQUENCY_A::K125;
+    let spi = Spi::new(board.SPIM0, );
 }
