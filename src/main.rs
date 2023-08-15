0cb132a0192f2672270b6be9194665295cd5899c
Bart Massey <bart.massey@gmail.com>
2023-08-15 14:52:52 -0700

    got spi setup to compile

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,40 @@
 #![no_std]
 
 use panic_rtt_target as _;
-use rtt_target::{rprintln, rtt_init_print};
+use rtt_target::rtt_init_print;
 
 use cortex_m_rt::entry;
 use microbit::{
     board::Board,
-    hal::{spi::Spi, spim},
-    pac::spim0,
+    hal::{gpio, spi, spim},
+    pac::{self, spi0::frequency},
 };
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
+    let p = pac::Peripherals::take().unwrap();
     let board = Board::take().unwrap();
     let spi_mode = spim::Mode {
         polarity: spim::Polarity::IdleLow,
         phase: spim::Phase::CaptureOnFirstTransition,
     };
     // XXX Can probably be much faster.
-    let spi_clock_rate = spim0::FREQUENCY_A::K125;
-    let spi = Spi::new(board.SPIM0, );
+    let spi_clock_rate = frequency::FREQUENCY_A::K125;
+    let sck = board.pins.p0_17
+        .into_push_pull_output(gpio::Level::Low)
+        .degrade();
+    let mosi = board.pins.p0_13
+        .into_push_pull_output(gpio::Level::Low)
+        .degrade();
+    let spi_pins = spi::Pins {
+        sck,
+        mosi: Some(mosi),
+        miso: None,
+    };
+    let _spi = spi::Spi::new(p.SPI0, spi_pins, spi_clock_rate, spi_mode);
+
+    loop{
+        continue;
+    }
 }
